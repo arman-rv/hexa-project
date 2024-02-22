ed886ff21ed9da719fe3bfde0d1760bdb8137995
arman-rv <pendingcoding@gmail.com>
2024-02-22 14:29:21 +0330

    error handling

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 const LoadingSpinner = () => {
     const [loadingHidden, setLoadingHidden] = useState(false)
--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { HeroSection , Services , CourseEducation,Categories ,Suggestions ,OurTeachers ,News } from '../../components/Landing';
 import LoadingSpinner from '../../components/common/loadingSpinner';
+
 const Landing = () => {
 
   return (
